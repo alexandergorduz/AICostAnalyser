63c73efdb61b05e70235a05864ca9f3ab72e7f1d
Wanackon <wanackon758@gmail.com>
2025-05-09 20:45:47 +0300

    Add command handling to bot: set '/start' command with description for user interaction.

--- bot.py
+++ bot.py
@@ -1,30 +1,31 @@
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from config import BOT_TOKEN
 from utils.database import initialize_database
 from handlers.commands import router as commands_router
 from handlers.messages import router as messages_router
 
-
-
 logging.basicConfig(level=logging.INFO)
-
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-
 async def main() -> None:
 
     initialize_database()
+
+    commands = [
+        BotCommand(command="start", description="🚀 Запустити бота / Перезавантажити"),
+    ]
+    await bot.set_my_commands(commands)
 
     dp.include_router(commands_router)
     dp.include_router(messages_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
 
